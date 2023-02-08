528beca4ae6bd75ecee7eb80168f596857bedfa8
ktanoooo <ktanoooo1112@gmail.com>
2023-02-08 18:52:42 +0900

    Consider profile

--- scripts/rehydrate.sh
+++ scripts/rehydrate.sh
@@ -4,7 +4,12 @@ set -eu
 
 read -p "Date (YYYYMMDD): " DATE
 read -p "Hour (00-23): " HOUR
+read -p "aws profile (default): " PROFILE
 
 BACKET="bucket-name-datadog-log"
 
-aws s3 ls $URL | fzf | awk '{print $4}' | xargs -I{} aws s3 cp s3://$URL{} - | zcat | gojq -r '.message'
+if [ -z "$PROFILE" ]; then
+  PROFILE="default"
+fi
+
+aws --profile $PROFILE s3 ls $URL | fzf | awk '{print $4}' | xargs -I{} aws --profile $PROFILE s3 cp s3://$URL{} - | zcat | gojq -r '.message'
